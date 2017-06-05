2bc1e9c0fa01ed588a7ba8799f33a7adaba00566
qiu <423822728@qq.com>
2017-06-05 21:25:18 +0800

    优化js压缩配置

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,8 @@ const config = {
     }
   },
   module: {
+    //忽略以下js
+    noParse: /node_modules\/(jquey|moment|chart\.js)/,
     rules: [
       {
         test: /\.vue$/,
@@ -146,17 +148,9 @@ const config = {
 
   ],
   devServer: {
-    host: myHost,
     port: 8010,
     historyApiFallback: false,
     noInfo: true,
-    proxy: {
-      '/api': {
-        target: 'http://' + myHost + ':8010',
-        changeOrigin: true,
-        pathRewrite: { '^/api': '' }
-      }
-    }
   },
   devtool: '#eval-source-map'
 }
@@ -194,8 +188,20 @@ if (process.env.NODE_ENV === 'production') {
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
+      // 最紧凑的输出
+      beautify: false,
+      // 删除所有的注释
+      comments: false,
       compress: {
-        warnings: false
+        // 在UglifyJs删除没有用到的代码时不输出警告  
+        warnings: false,
+        // 删除所有的 `console` 语句
+        // 还可以兼容ie浏览器
+        drop_console: true,
+        // 内嵌定义了但是只用到一次的变量
+        collapse_vars: true,
+        // 提取出出现多次但是没有定义成变量去引用的静态值
+        reduce_vars: true,
       }
     })
   ]),
