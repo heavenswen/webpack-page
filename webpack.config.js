751bac64fdd934768f8e46c11840ee3083b76555
Heavenswen <423822728@qq.com>
2017-06-06 09:58:37 +0800

    不能用

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const webpack = require('webpack')
 const glob = require('glob')
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const { WebPlugin, AutoWebPlugin } = require('web-webpack-plugin')
+
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
@@ -126,12 +129,12 @@ const config = {
   },
   plugins: [
     //获取公用模块生成js
-    new CommonsChunkPlugin({
-      name: 'vendors',
-      filename: 'assets/js/vendors.js?[hash]',
-      chunks: chunks,
-      minChunks: chunks.length
-    }),
+    // new CommonsChunkPlugin({
+    //   name: 'vendors',
+    //   filename: 'assets/js/vendors.js?[hash]',
+    //   chunks: chunks,
+    //   minChunks: chunks.length
+    // }),
     //提取公用模块生成css
     new ExtractTextPlugin({
       filename: (getPath) => {
@@ -141,10 +144,62 @@ const config = {
           let arr = name.split('/')
           name = arr[arr.length - 2]//获得文件名
         }
+        console.log(name)
         return 'assets/css/' + name + '.css';
       },
       allChunks: true
     }),
+    new AutoWebPlugin(
+      // 所有页面的入口目录
+      './src/pages/user',
+      {
+        // 以下所有的属性都不是必须的，按照需要选填  
+
+        // {string,function}
+        // 所有页面采用的模版文件
+        // 如果 template 的类型是 string，template代表模版文件的相对于当前目录根目录的全文件路径
+        // 如果 template 的类型是 function，template代表可自定义逻辑的函数 function(pageName)=>newFullPath，告诉你当前页面名称你返回一个新的路径代表当前页面的模版路径
+        template(p) {
+          return "./src/pages/user/" + p + "/app.ejs"
+
+        },
+
+        // 当前页面的javascript入口文件，如果为空就使用当前page目录下的 index.js 作为入口
+        // 如果 entry 的类型是 string，entry代表入口文件的相对于当前目录根目录的全文件路径
+        // 如果 entry 的类型是 function，entry代表可自定义逻辑的函数 function(pageName)=>newFullPath，告诉你当前页面名称你返回一个新的路径代表当前页面的入口路径
+        entry(p) {
+
+          return "./src/pages/user/" + p + "/app.js"
+        },
+
+        // {function}
+        // 每个page输出的html的名称，默认按照文件夹名称作为html名称，
+        // 设置 filename 为 function(pageName)=>filename 类型 可添加自定义逻辑
+        filename: null,
+
+        // 提取出所有页面公共的代码,如果为空就不做提取操作。
+        // 透传给 `CommonsChunkPlugin` 插件的属性
+        commonsChunk: {
+          name: 'vendors',// 必填属性,输出的文件名称
+          minChunks: 2,// 来自 CommonsChunkPlugin 插件
+        },
+
+        // 在所有入口页面的entry前插入
+        // preEntrys: ['./path/to/file1.js'],
+
+        // 在所有入口页面的entry后插入
+        // postEntrys: ['./path/to/file2.js'],
+
+        // {string} publicPath for css file,for js file will use webpack.publicPath
+        // stylePublicPath: null,
+
+        // page name list will not ignore by AutoWebPlugin(Not output html file for this page name)
+        ignorePages: ['pageName'],
+
+        // 是否输出一个名叫 pagemap.json 的文件，这个文件包含所有被AutoWebPlugin解析到的2入口页面，文件格式如下
+        // {"page name": "page url",}
+        outputPagemap: true,
+      }),
 
   ],
   devServer: {
@@ -155,6 +210,7 @@ const config = {
   devtool: '#eval-source-map'
 }
 
+//建立HtmlWebpackPlugin 模版
 glob.sync("./src/pages/**/*.{ejs,html}").forEach(path => {
   //HtmlWebpackPlugin 不支持 .html 编译 ejs 用.ejs
   let filename = path.split('./src/pages/')[1].split(/\/app.(ejs|html)/)[0]
@@ -173,7 +229,7 @@ glob.sync("./src/pages/**/*.{ejs,html}").forEach(path => {
     env: process.env.NODE_ENV === 'production',//HtmlWebpackPlugin.options.env 非打包时的处理
     chunks: ['vendors', chunk] //chunk
   }
-  config.plugins.push(new HtmlWebpackPlugin(htmlConf))
+  // config.plugins.push(new HtmlWebpackPlugin(htmlConf))
 })
 
 module.exports = config
