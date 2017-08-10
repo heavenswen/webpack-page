d6cf5dd84a6f9a89748851ece8101f03bd5f3f9c
Heavenswen <423822728@qq.com>
2017-08-10 09:35:32 +0800

    404 页面

--- src/components/false.vue
+++ src/components/false.vue
@@ -1,6 +1,6 @@
 <template>
-    <section class='page page404' @click='back'>
-        <div class='text-center'>
+    <section class='page page404'>
+        <div class='text-center'  @click='back'>
             <h1>404</h1>
             <p>页面不见了ヾ(;ﾟ;Д;ﾟ;)ﾉﾞ</p>
             <p class='small'>点击返回</p>
@@ -9,11 +9,9 @@
 </template>
 
 <script>
-// import "assets/css/404.scss"
+import "assets/css/404.scss"
 export default {
-    watch: {
 
-    },
     methods: {
         //触发路由上1page
         back() {
