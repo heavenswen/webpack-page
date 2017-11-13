0bdc89997089e6d5fda5d9d4da31fe75098c768a
Heavenswen <423822728@qq.com>
2017-11-13 15:56:15 +0800

    仿网易云搜索效果

--- src/assets/js/search-wyy.js
+++ src/assets/js/search-wyy.js
@@ -1,129 +1,195 @@
 /**
- * @file  仿网易云搜索UI
+ * @file  仿网易云搜索UI 菜单会直接插入父层定位容器中
  * @author qiu(423822728@qq.com)
+ * @class
  */
 ; (function () {
 
-    var searchWYY = function (seleter, obj) {
-        //触发器
-        this.dom = document.querySelector(seleter);
+    /**
+     * 初始化
+     * @param {string} seleter  css选择器 
+     * @param {Object} object 配置 
+     * @param {number} object.margin 上边距
+     * @param {width} object.number 显示宽度
+     * @param {Object} object.keys 数据key
+     * @param {string}  object.keys.title 分类标题
+     * @param {string}  object.keys.href 分类链接
+     * @param {array}  object.keys.list 分类子集
+     * @param {string}  object.keys.list 分类子集
+     * @param {function=} object.callback 返回数据给组件
+     */
+    var searchWYY = function (seleter, object) {
 
-        //更多参数
-        function exend(obj) {
-            //默认设置
-            let json = {
-                //默认默认为搜索宽宽度
-                margin: '0',
-                width: that.xy.w,
-                //数据字段
+        //触发对象
+
+        this.trigger = document.querySelector(seleter);
+
+
+        // 获得目标参数
+        this.xy = {
+            //
+            h: this.trigger.clientHeight,
+            //作为显示栏宽度
+            w: this.trigger.clientWidth,
+            //get xy relative to parent
+            y: this.trigger.offsetTop,
+            x: this.trigger.offsetLeft
+        };
+
+        /**
+         * 默认配置
+         * @const
+         * @type {Obejct} 
+         */
+        var CONFIG = {
+            //默认默认为搜索宽宽度
+            margin: 0,
+            width: this.trigger.clientWidth,
+            //数据字段
+            keys: {
+
                 title: 'title',//分类
                 href: 'href',//分类链接
                 list: "list",//子类列表
-                cell: 'value',//子类名称
+                cell: 'title',//子类名称
                 cellHref: 'href',//子类链接
-            }
-            //过滤
-            let data = obj || {};
-            for (let key in json) {
-                if (!data[key]) data[key] = json[key]
-            }
+            },
+            //触发回调 输入宽的值，和执行生成的方法
+            callBack: function (val, callbackFn) {
 
-            return data;
+                console.log("init callBack function")
+            },
+
         }
-        //容器层
-        this.parent = this.dom.offsetParent;
-        // 获得目标参数
-        this.xy = {
-            h: this.dom.clientHeight,
-            //作为显示栏宽度
-            w: this.dom.clientWidth,
-            //get xy relative to parent
-            y: this.dom.offsetTop,
-            x: this.dom.offsetLeft
-        };
+
+        if (!object) object = {};
+        //合并同类项
+        for (var k in CONFIG) {
+            object[k] = object[k] ? object[k] : CONFIG[k];
+        }
+        this.CONFIG = object;
+
+        //获得触发对象的上层容器
+        this.parent = this.trigger.offsetParent;
+
+        //菜单对象生成
         //content dom 展示框
-        this.htm = document.createElement("section");
+        this.obj = document.createElement("section");
         //添加样式组
-        this.htm.classList.add("search-com");
+        this.obj.classList.add("search-com");
 
-        //获得 数据字段名 
+        this.parent.appendChild(this.obj);
         var that = this;
+        /**
+         * 显示
+         * @event input
+         */
+        this.trigger.addEventListener("input", function () {
+            //获得输入的值
+            var val = this.value;
+            if (!val) {
+                that.empty();
+                return;
+            }
+            //回调输入的参数 返回数据
+            var json = that.CONFIG.callBack(val);
+            var htm = that.addDomNav(val, json)
+            that.obj.innerHTML = htm;
+            that.objPosition();
 
-        var keys = exend(obj);
+            that.obj.classList.add("isshow");
 
-        //显示菜单
-        function initSearch() {
-            let value = this.value
-            //为空时删除
-            if (!value) {
-                return that.remove();
-            };
-            //渲染数据
-            that.append(value, dataJson);
-        }
+            /**
+             * a链接不冒泡到blur
+             * @event click
+             */
+            var as = that.obj.querySelectorAll("a");
+            for (var i = 0; i < as.length; i++) {
+                var a = as[i];
+                a.addEventListener("click", function (e) {
+                    e.stopPropagation();
+                })
+            }
 
-        var trigger = new Event("trigger", { "bubbles": true, "cancelable": false });
-
-        this.dom.addEventListener("trigger", initSearch)
-
-        this.dom.addEventListener("input", function () {
-            this.dom.dispatchEvent(trigger)
+        })
+        /**
+         * 显示
+         * @event focus
+         */
+        this.trigger.addEventListener("focus", function () {
+            that.obj.classList.add("isshow");
         })
 
+        /**
+         * 隐藏
+         * @event focus
+         */
+        this.trigger.addEventListener("blur", function () {
+            that.obj.classList.remove("isshow");
+        })
+    }
 
-        //生成展示框 (查询的值,数据) 
-        function addHtmFn(value, json) {
-            let htm = '<p class="search-tip">搜索“' + value + '”相关内容</p>';
-            //标注
-            let reg = RegExp(value, 'ig');
-            //class
-            for (let k in json) {
-                //item
-                let cls = json[k];
-                let ul = '';
-                //获得详情
 
-                for (let i = 0; i < cls[keys.list].length; i++) {
-                    let li = cls[keys.list][i]
-                    let content = li[keys.cell];
-                    //标记关键字
-                    content = content.replace(reg, "<span class='search-keys'>" + value + "</span>");
-                    ul += " <li><a href = '" + li[keys.cellHref] + "'>" + content + "</a></li>"
+    /**
+     * 定位显示框
+     * 
+     */
+    searchWYY.prototype.objPosition = function () {
+        this.obj.style.width = this.CONFIG.width + "px";
+        var top = Number(this.xy.y) + Number(this.xy.h) + Number(this.CONFIG.margin);
+        this.obj.style.top = top + "px";
+        this.obj.style.left = Number(this.xy.x) + "px";
+    }
 
-                }
+    /**
+     * 生成菜单组件
+     * @param {string} val 搜索的值 
+     * @param {Obeject} json 列表数组
+     * @event
+     * @public
+     */
+    searchWYY.prototype.addDomNav = function (value, json) {
+        //标题
+        var htm = '<p class="search-tip">搜索“' + value + '”相关内容</p>';
+        //标注
+        var reg = RegExp(value, 'g');
+        //分类
+        for (var k in json) {
+            //一维数据
+            var data = json[k];
 
-                htm += "<div class=\"search-item\">"
-                    +
-                    "<h4 class='search-tit'><a href='" + cls[keys.href] + "'>" + cls[keys.title] + "</a></h4><ul class='search-cell'>" +
-                    ul
-                    + "</ul></div>";
+            var ul = '';
+            //生成菜单详情
+            for (var i = 0; i < data[this.CONFIG.keys.list].length; i++) {
+                var li = data[this.CONFIG.keys.list][i]
+                var content = li[this.CONFIG.keys.cell];
+                //标记关键字
+                content = content.replace(reg, "<span class='search-keys'>" + value + "</span>");
+                ul += " <li><a href = '" + li[this.CONFIG.keys.cellHref] + "'>" + content + "</a></li>"
 
             }
+            //生成 搜索框列表容器
+            htm += "<div class=\"search-item\">"
+                + "<h4 class='search-tit'>"
+                + "<a href='" + data[this.CONFIG.keys.href] + "'>"
+                + data[this.CONFIG.keys.title]
+                + "</a>"
+                + "</h4>"
+                + "<ul class='search-cell'>"
+                + ul
+                + "</ul>"
+                + "</div>";
 
-            return htm;
+        }
+        return htm;
+    }
 
-        };
-        //trigger 生成 搜索框
-        this.append = function (value, json) {
-            console.log(keys)
-            this.htm.style.width = this.xy.w + "px";
-            this.htm.style.top = Number(this.xy.y) + Number(this.xy.h) + Number(keys.margin) + "px";
-            this.htm.style.left = Number(this.xy.x) + "px";
-            this.htm.innerHTML = addHtmFn(value, json)
-            //添加dom
-            this.parent.appendChild(this.htm)
-        }
-        //自定义dom
-        this.addDom = function (dom) {
-            this.htm.innerHTML = dom;
-            //添加dom
-            this.parent.appendChild(this.htm)
-        }
-        //删除 搜索框            
-        this.remove = function () {
-            this.htm.remove();
-        }
-
+    /**
+     * 删除 搜索框组件   
+     *@public
+     */
+    searchWYY.prototype.empty = function () {
+        this.obj.innerHTML = '';
     }
 
     window.searchWYY = searchWYY
--- src/entry/demo.js
+++ src/entry/demo.js
@@ -1,6 +1,7 @@
 import 'assets/css/react.scss'
 
 //仿网易云音乐搜索
+import  "assets/css/search-wyy.css"
 import Search from "assets/js/search-wyy"
 
 //焦点提示框
@@ -8,7 +9,41 @@ import 'assets/css/Focus-tip.css'
 import Tip from "assets/js/Focus-tip"
 
 //搜索框
-var search = new Search("#search");
+var search = new Search("#search", {
+    callBack: function (v, fn) {
+
+        return [
+            {
+                title: "视频",
+                href: "#",
+                list: [
+                    {
+                        title: "3d制作",
+                        href: "url"
+                    },
+                    {
+                        title: "3d制作",
+                        href: "url"
+                    }
+                ]
+            },
+            {
+                title: "视频",
+                href: "#",
+                list: [
+                    {
+                        title: "3d制作",
+                        href: "url"
+                    },
+                    {
+                        title: "3d制作",
+                        href: "url"
+                    }
+                ]
+            },
+        ];
+    }
+});
 
 //焦点提示
 var tip = new Tip();
