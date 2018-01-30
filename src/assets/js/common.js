9b433024cfd05f0611e573ff5a2831b00f20b78f
qiu <423822728@qq.com>
2018-01-30 15:11:19 +0800

    classList j兼容方法

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -4,6 +4,93 @@
  * 采用es6 编译 在线编译 http://google.github.io/traceur-compiler/demo/repl.html#const%20a%20%3D%201%0Alet%20b%20%3D%201%0Avar%20c%20%3D%20%60123%24%7Bb%7D%60
  */
 ; (function () {
+    // classList 兼容 ie9+
+    if (!("class1" in document.documentElement)) {
+        Object.defineProperty(HTMLElement.prototype, 'class1', {
+            get() {
+                //获得当前DOM节点
+                let obj = this;
+
+                /**
+                 * 处理函数
+                 * @param {Function} 回调具体执行方法
+                 * @return {Function}  
+                 */
+                //数组所有classname
+                let classes = obj.className.split(/\s+/g)
+                function update(fn) {
+                    // 暴露接口
+                    return function (value) {
+                        //当前index
+                         let  index = classes.indexOf(value);
+                        //type 处理
+                        fn(index, value);
+                        //合并数组classname
+                        if(classes)obj.className = classes.join(" ");
+                    }
+                }
+                /**
+                 * classList 对象
+                 * @namespace
+                 */
+                class ClassObj extends Array {
+                    constructor(...val) {
+                        super(...val);
+                        this.value = obj.className//classList.value 真实的值 "classname classname"
+                        //添加
+                        this.add = update(function (index, value) {
+                            if(classes.length==1&&classes[0] == '')classes = [];
+                                //按位取反 布尔取反 
+                                if (!~index) classes.push(value);
+                        })
+                        // 删除
+                        this.remove = update(function (index) {
+                                //按位取反 ~-1 == 0
+                                if (~index) classes.splice(index, 1);
+                        })
+                        // 切换
+                        this.toggle = update(function (index, value) {
+                            //toggle 时只能有一个值
+                            if (~index){
+                                classes.splice(index, 1);
+                            }
+                            else{
+                                   classes.push(value);
+                            }
+                        })
+                        // 检查
+                        this.contains = function (value) {
+                            //~0 == -1 !!-1 == true 
+                            return !!~obj.className.split(/\s+/g).indexOf(value);
+                        }
+                        //获取指定
+                        this.item = function (i) {
+                            return obj.className.split(/\s+/g)[i] || null;
+                        }
+                    }
+                }
+
+                return new ClassObj(...classes);
+            }
+        });
+    }
+    //dataset 兼容
+    if (!("dataset" in document.documentElement)) {
+        Object.defineProperty(HTMLElement.prototype, 'dataset', {
+            get() {
+                //获得当前DOM节点
+                let obj = this
+
+                return {}
+            }
+
+
+        })
+    }
+
+    /**
+     * @namespace
+     */
     const COMMON = {
         winHref: window.location.href,
         /** 
@@ -16,7 +103,7 @@
             //父级
             let list = dom.parentNode.querySelectorAll(traget)
             let arr = []
-            for (let obj of list ) {
+            for (let obj of list) {
                 if (obj != dom) {
                     arr.push(obj);
                 }
@@ -81,7 +168,7 @@
          * @param {string} active className 默认“active”
          * @param {Function} fn 回调事件
          */
-        navActive(traget, find = "a", active = 'active',fn) {
+        navActive(traget, find = "a", active = 'active', fn) {
             //当前链接
             let win = this.winHref
             //获得导航列表组
